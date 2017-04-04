add is_catalyst in the RFReactants class, to avoid wrong utilities calculation; calculation emission only if there are data from factory_emision table; calculate utility only if there are data from factory_reaction_utility table;

CE_webclient/public/services/ce_analysis.py
@@ -41,15 +41,15 @@ class CEAnalysis:
         """
         # all the object_id + offset : internal index
         self.dict_obj_id_2_index = dict()
-        # store the row_index : object_id (no offset)
+        # store the row_index : object_id + offset)
         self.dict_row_index_2_obj_id = dict()
-        # store the col_index: object_id (no offset)
+        # store the col_index: object_id + offset)
         self.dict_col_index_2_obj_id = dict()
 
         # row: factory
         row_index = itertools.count(0)
         for factory_obj_id in all_factory.keys():
-            unique_factory_id = CEAnalysis.OFFSET_FACTORY + factory_obj_id
+            unique_factory_id = CEAnalysis.__generate_unique_obj_id(factory_obj_id, SHORT_NAME_FACTORY)
             index = next(row_index)
             self.dict_obj_id_2_index[unique_factory_id] = index
             self.dict_row_index_2_obj_id[index] = unique_factory_id
@@ -57,23 +57,23 @@ class CEAnalysis:
         # column: chemicals
         col_index = itertools.count(0)
         for chem_obj_id in all_chemical.keys():
-            unique_chem_id = CEAnalysis.OFFSET_CHEMICAL + chem_obj_id
+            unique_chem_id = CEAnalysis.__generate_unique_obj_id(chem_obj_id, SHORT_NAME_CHEMICAL)
             index = next(col_index)
             self.dict_obj_id_2_index[unique_chem_id] = index
             self.dict_col_index_2_obj_id[index] = unique_chem_id
 
         # column: utility types
         for utility_object_id in all_utility_type.keys():
-            unique_utility_id = CEAnalysis.OFFSET_UTILITY_TYPE + utility_object_id
+            unique_utility_id = CEAnalysis.__generate_unique_obj_id(utility_object_id, SHORT_NAME_UTILITY_TYPE)
             index = next(col_index)
             self.dict_obj_id_2_index[unique_utility_id] = index
             self.dict_col_index_2_obj_id[index] = unique_utility_id
 
         # column: emissions, use emission name, not the object_id, since different object_id may point to the same
-        # emission
+        # emission. The emission data is per reaction_formula related
         for emission_per_rf in all_emission.values():
             for emission_data in emission_per_rf:
-                emission_name = CEAnalysis.PREFIX_EMISSION + emission_data.name
+                emission_name = CEAnalysis.__generate_unique_obj_id(emission_data.name, SHORT_NAME_EMISSION)
                 if emission_name not in self.dict_obj_id_2_index:
                     index = next(col_index)
                     self.dict_obj_id_2_index[emission_name] = index
@@ -86,7 +86,7 @@ class CEAnalysis:
         self.A = np.zeros((n_rows, n_cols), dtype=np.float64)
 
     def get_factory_id_by_index(self, row_index):
-        object_id = self.dict_row_index_2_obj_id.get(row_index)
+        object_id = self.dict_row_index_2_obj_id.get(row_index) - CEAnalysis.OFFSET_FACTORY
         return object_id
 
     def get_object_id_by_index(self, col_index):
@@ -104,12 +104,8 @@ class CEAnalysis:
             # since the column name for emission starts with 'emis_', so we return a substring of the column name
             return object_id[5:], SHORT_NAME_EMISSION
 
-    def get_index_by_id(self, object_id, name):
-        """
-        :param object_id: object_id or name(of emission) of the component from database
-        :param name: indication of factory, emission ,utility and chemical
-        :return: index of one axis of the 2D array 
-        """
+    @staticmethod
+    def __generate_unique_obj_id(object_id, name):
         unique_obj_id = object_id
         if name.lower() == SHORT_NAME_CHEMICAL:
             unique_obj_id += CEAnalysis.OFFSET_CHEMICAL
@@ -121,12 +117,36 @@ class CEAnalysis:
             unique_obj_id = CEAnalysis.PREFIX_EMISSION + object_id
         else:
             print("[Warning]: unknown name", name)
-            return None
+            raise ValueError("Unknown name to generate the column index")
+        return unique_obj_id
+
+    def get_index_by_id(self, object_id, name):
+        """
+        :param object_id: object_id or name(of emission) of the component from database
+        :param name: indication of factory, emission ,utility and chemical
+        :return: index of one axis of the 2D array 
+        """
+        unique_obj_id = CEAnalysis.__generate_unique_obj_id(object_id, name)
         # for emission, the object_id is actually the emission name, not an integer
         index = self.dict_obj_id_2_index.get(unique_obj_id)
         if index is None:
-            print("[Warning]: no index found for ", unique_obj_id)
+            raise ValueError("[Error]: no index found for " + str(unique_obj_id))
         return index
+
+    def get_factory_ids_by_col_id(self, col_object_id, name):
+        """
+        with a known col_object_id, indicating as a chemical, emis, utility, return a list of factory id's
+        :param col_object_id: 
+        :param name: 
+        :return: a list of factory_id which produce/supply the chemical or service indicated by the col_object_id
+        """
+        col_index = self.get_index_by_id(col_object_id, name)
+        # in the array column, get all the entries whose value > 0, which means the factory produce/supply
+        # this product or service
+        a_column = self.A[:, col_index]
+        row_indices = np.where(a_column > 0)
+        factory_ids = [self.get_factory_id_by_index(i) for i in np.nditer(row_indices)]
+        return factory_ids
 
     def set_value(self, factory_obj_id, col_object_id, name, value):
         """       
@@ -140,4 +160,10 @@ class CEAnalysis:
         col_index = self.get_index_by_id(col_object_id, name)
         self.A[row_index, col_index] += value
 
+    @property
+    def json_format(self):
+        return {'id2idx': self.dict_obj_id_2_index,
+                'rIdx2id': self.dict_row_index_2_obj_id,
+                'cIdx2id': self.dict_col_index_2_obj_id
+                }
 