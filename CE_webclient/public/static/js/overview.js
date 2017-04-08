879f3ca480d9c855dfeb312c053794c70b8b0880
Han Zhou <han.zhou@tno.nl>
2017-04-08 20:04:51 +0200

    get the total area profit

---
+++ CE_webclient/public/static/js/overview.js
@@ -0,0 +1,17 @@
+﻿var OVERVIEW = (function () {
+    return {
+        show_area_total_revenue: function () {
+            $.getJSON(url_whole_area_revenue)
+            .done(function (data) {
+                if (data) {
+                    var total_value = data[0];
+                    var value_unit = data[1];
+                    $("#area_total_revenue").text("已知区域内总净收入: " + total_value + " " + value_unit);
+                }
+            })
+            .fail(function (status, err) {
+
+            })
+        }
+    }
+})();
